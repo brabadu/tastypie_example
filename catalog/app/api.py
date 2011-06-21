ca4538c24b34ee963cb7ab809b9d1fb687ee631a
Boryslav Larin <brabadu@gmail.com>
2011-06-21 11:19:54 +0300

    Added Folders to response

--- catalog/app/api.py
+++ catalog/app/api.py
@@ -1,8 +1,21 @@
 from tastypie.resources import ModelResource
+from tastypie import fields
 
-from app.models import Cabinet
+from app.models import Cabinet, Folder
+
+
+class FolderResource(ModelResource):
+    class Meta:
+        queryset = Folder.objects.all()
+        excludes = ['id']
+        include_resource_uri = False
 
 
 class CabinetResource(ModelResource):
+    folders = fields.ToManyField('app.api.FolderResource', 'folders', full=True)
+
     class Meta:
         queryset = Cabinet.objects.all()
+        resource_name = 'cabinet/list'
+        excludes = ['id']
+        include_resource_uri = False
