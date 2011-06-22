b998502e6faed72371a96d8ba740dae551e87b69
Boryslav Larin <brabadu@gmail.com>
2011-06-22 23:18:50 +0300

    Fixed renaming 'catalog' to 'src'

--- src/settings.py
+++ src/settings.py
@@ -100,7 +100,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'catalog.urls'
+ROOT_URLCONF = 'src.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
